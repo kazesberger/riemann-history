5c83d1d31180f3a1b4b89f1f45938304eecee032
kazesberger <kazesberger@gmail.com>
2018-06-23 23:46:18 +0200

    wip

--- project.clj
+++ project.clj
@@ -6,8 +6,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[clojurewerkz/elastisch "3.0.0"]
+                 [cheshire "5.8.0"]
+                 [org.clojure/core.async "0.4.474"]
+                 [tea-time "1.0.0"]
                  [jarohen/chime "0.2.2"]]
-
 
   :profiles {:dev {:dependencies [[riemann "0.3.1"]
                                   [org.clojure/clojure "1.9.0"]]}}
--- src/riemann_history/elastic.clj
+++ src/riemann_history/elastic.clj
@@ -1,11 +1,30 @@
 (ns riemann-history.elastic
   (:require [clj-time.core :as t]
             [clj-time.periodic :refer [periodic-seq]]
-            [chime :as chime]))
-;
+            [cheshire.core :as cheshire]
+            [clojure.core.async :as a]
+            [tea-time.core :as tt]
+            [chime :refer :all]))
 
-(take 5 (periodic-seq
-          (t/now)
-          (-> 5 t/minutes)))
+(let [chimes (chime-ch [(-> 2 t/seconds t/from-now)
+                        (-> 3 t/seconds t/from-now)])]
+  (a/<!! (go-loop []
+                  (when-let [msg (<! chimes)]
+                    (prn "Chiming at:" msg)
+                    (recur)))))
 
-;(take 5 (-> 2 t/seconds t/from-now))
+(chime-ch (take 5 (periodic-seq
+                    (t/now)
+                    (-> 5 t/minutes))))
+
+
+;(def fred-durst (tt/every! 10 2 (bound-fn [] (prn "THAT is a thing"))))
+
+;;;;;;;;;;;;;;;;;;;;;;
+(require 'clojure.java.io)
+(def query-result
+  (cheshire/parse-string
+    (slurp (clojure.java.io/resource "elasticsearch/result.example"))
+    keyword))
+;;;;;;;;;;;;;;;;;;
+
